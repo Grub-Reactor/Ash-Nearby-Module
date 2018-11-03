d853c00f94a5947e69dc6077c236e6acb0a39746
Ashcoca <ashvincoca@gmail.com>
2018-11-03 00:08:33 -0700

    still needs work, temp final

--- client/src/components/card.jsx
+++ client/src/components/card.jsx
@@ -13,26 +13,21 @@ class Card extends React.Component {
     }
   }
 
-
   toggleClass() {
     const currentState = this.state.active;
     this.setState({
       active: !currentState });
   };
 
-  generateStars(num) {
-    const starPercentage = (num / 5) * 100;
-    const starPercentageRounded = `${(Math.round(starPercentage / 10) * 10)}%`;
-    // document.querySelector(`.stars-inner`).style.width = starPercentageRounded; 
-    return;
-  }
-
-  handleMouseOver() {
-
+  onMouseEnter() {
+    this.setState(toggleHover()
+    )
   }
 
   toggleHover() {
-    
+    return {
+      hover: !this.state.hover
+    };
   }
   
 
@@ -45,12 +40,10 @@ class Card extends React.Component {
                 <div className="favorite"> 
                 <img className="rest-image"src={rests.restaurantCard.imageURL}></img>
                 <svg id="ribbon" className={this.state.active ? "ribbon-active" : "ribbon-inactive"}  onClick={this.toggleClass.bind(this)} viewBox="0 0 24 24"><path d="M0 0h24v24H0z" fill="none"></path><path d="M5 2.98v18.1L12 16l7 5.08V2.98H5zm0 0" fill="currentColor"></path></svg>
-                  {/* <i className={this.state.active ? "fa fa-bookmark-o hidden" : "fa fa-bookmark-o" ? "fa fa-bookmark-o" : "fa fa-bookmark-o hidden" } aria-hidden="true" onClick={this.toggleClass.bind(this)}></i>
-                  <i className={this.state.active ? "fa fa-bookmark" : "fa fa-bookmark hidden" ? "fa fa-bookmark hidden" : "fa fa-bookmark"} aria-hidden="true" onClick={this.toggleClass.bind(this)}></i> */}
                 </div>
             </div>
             <div className="card-bottom">
-              <div className="card-title">
+              <div className="card-title" onMouseEnter={<Hover props={rests}></Hover>}>
                 <h4 className="rest-name">{rests.restaurantCard.restaurantName}</h4>
                 <div className="cuisines">{rests.restaurantCard.cuisine}</div>
               </div>
@@ -60,7 +53,7 @@ class Card extends React.Component {
                   <div className="total-reviews">{rests.restaurantCard.totalReviews} ratings</div>
                 </div>
                 <div className="small-card-right">
-                  <div className="stars-outer">{this.generateStars(rests.restaurantCard.starReviews)}</div>
+                  <div className="stars-outer"></div>
                     <div className="stars-inner"></div>
                   <div className="minimum">${rests.restaurantCard.deliveryMin} min.</div>
                 </div>
