d737bf810e0f52789e0bfb872db099f26a9dc6c5
Ashcoca <ashvincoca@gmail.com>
2018-11-05 14:33:03 -0800

    Converted to styled components

--- client/src/components/hover.jsx
+++ client/src/components/hover.jsx
@@ -1,7 +1,17 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const Hoverings = styled.div ` 
+position: absolute;
+opacity: 0;
+  &:hover {
+    opacity: 1;
+    transition-duration: 0.25s;
+  }
+`
 
 const Hover = ({ onHover }) => (
-  <div className="hover">{onHover}</div>
+  <Hoverings>{onHover}</Hoverings>
 )
 
 export default Hover;
